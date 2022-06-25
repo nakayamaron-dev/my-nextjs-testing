81d861a2643d5523b06b0942c8223de16eb890c6
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-06-25 10:01:47 +0900

    impl blog-page testing

---
+++ pages/context-page.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const CntextPage: React.FC = () => {
+  return (
+    <Layout title="Comment">
+      <p className="text-4xl">context page</p>
+    </Layout>
+  )
+}
+
+export default CntextPage
